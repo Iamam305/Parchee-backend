32a911227d5a4984cfa1532d0fc7a986f28908ec
ajitesh <ajitesh314mishra@gmail.com>
2022-09-02 23:20:31 +0530

    l

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,33 @@
-const express = require('express')
+const express = require("express");
+const { body, validationResult } = require('express-validator');
+const router = express.Router();
+const User = require("../models/User");
 
-const router = express.Router()
+router.post(
+  "/",
+  
+    body("name").isLength({ min: 5 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  
+  (req, res) => {
 
-router.get('/', (req, res) =>{
-res.json('auth')
-})
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() }); 
+    }
 
-module.exports = router
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    }).then((user) => res.json(user))
+    .catch((err) =>{
+        console.log(err);
+        res.json("email already in use")
+    });
+    res.json("auth");
+  }
+);
+
+module.exports = router;
